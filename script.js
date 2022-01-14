a6300b575f89ef1254dd4c8765af795b10ce6d58
unknown <bouyahyanada.solicode@gmail.com>
2022-01-14 15:45:24 +0100

    updated

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ for(var i = 0; i<21;i++){
     // var movieTitle = data[i]["title"];
     //     movieTitle = movieTitle.substring(0,10);
     //     movieTitle +="..."
-           content.innerHTML+= `<div class='col-4 container-fluid'> 
+           content.innerHTML+= `<div id ="allMovies" class='col-4 container-fluid'> 
                         <h4> ${data[i]["title"]}</h4> 
                         <img class='img-thumbnail' src=${data[i]["image"]}>
                         <p> ${data[i]["description"].substring(0,250)}...</p> 
